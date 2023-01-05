e11f2f92a1d4f69ba019e87e165b283e9f1a0f13
Yasmin Arnaut <yasminarnaut87@gmail.com>
2023-01-05 20:53:32 -0300

    incluindo so um usuario da lista e colocando um novo usuario na lista

--- src/main/java/com/devsuperior/userdept/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/userdept/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.devsuperior.userdept.repositories;
 
-public class UserRepository {
+import com.devsuperior.userdept.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
 }
